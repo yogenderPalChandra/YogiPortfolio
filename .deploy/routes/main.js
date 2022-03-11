71e968e3bf6df898c5849d1af480d1aa95e1b62b
Yogender <yogender027mae@gmail.com>
2022-03-11 20:29:02 +0100

    package.json changed

---
+++ .deploy/routes/main.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { route } = require("../app");
+const router = express.Router();
+
+// get handler for home paege ('/')of express router which takes
+// request response and next argument
+router.get("/", (req, res, next) => {
+  const data = req.context;
+  res.render("home", data);
+});
+router.get("/blog", (req, res, next) => {
+  const data = req.context;
+  res.render("blog", data);
+});
+
+router.get("/RaspberryPi", (req, res, next) => {
+  const data = req.context;
+  res.render("RaspberryPi", data);
+});
+
+router.get("/covid19", (req, res, next) => {
+  const data = req.context;
+  res.render("covid", data);
+});
+
+router.get("/flask", (req, res, next) => {
+  const data = req.context;
+  res.render("flask", data);
+});
+
+router.get("/dl", (req, res, next) => {
+  const data = req.context;
+  res.render("dl", data);
+});
+
+router.get("/blog1", (req, res, next) => {
+  const data = req.context;
+  res.render("blog1", data);
+});
+
+router.get("/re", (req, res, next) => {
+  const data = req.context;
+  res.render("renewableEnergy", data);
+});
+
+router.get("/eb", (req, res, next) => {
+  const data = req.context;
+  res.render("energyBuildings", data);
+});
+
+router.get("/se", (req, res, next) => {
+  const data = req.context;
+  res.render("solarEnergy", data);
+});
+module.exports = router;
